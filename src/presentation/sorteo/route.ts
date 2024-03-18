28f67f0764ee359ee1eb87421fa4d9173e3827a4
franz schwartz <gavino_10@hotmail.es>
2024-03-17 23:25:18 -0500

    new commit

---
+++ src/presentation/sorteo/route.ts
@@ -0,0 +1,28 @@
+import { Router } from 'express';
+import { SorteoController } from './controller';
+
+
+
+
+export class SorteoRoutes {
+
+
+  static get routes(): Router {
+
+    const router = Router();
+
+    const controller=new SorteoController();
+    // Definir las rutas
+    router.get('/', controller.getSorteos );
+    router.get('/', controller.getSorteosOne );
+    router.post('/', controller.postSorteos );
+    router.post('/:id', controller.patchSorteos );
+
+    router.delete('/:id', controller.deleteSorteos );
+
+
+    return router;
+  }
+
+
+}
